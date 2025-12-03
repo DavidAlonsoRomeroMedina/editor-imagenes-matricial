b7ed029275dff0a690826a2137655cb6c1fdd252
DavidAlonsoRomeroMedina <david.romero@tecdesoftware.edu.mx>
2025-12-03 08:54:59 -0600

    Update ejercicios.js

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,4 +1,4 @@
-// ============================================
+// ===========================================
 // EDITOR DE IMÁGENES CON ÁLGEBRA MATRICIAL
 // ============================================
 // Nombre del estudiante: David Alonso Romero Medina
